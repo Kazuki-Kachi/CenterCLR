51f92c5298318b373b18ea1520417c1730936e8e
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-07-23 13:00:28 +0900

    CreateProject

---
+++ CenterCLR/Module1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Console
+Imports System.Runtime.CompilerServices
+Imports CenterCLR.Extensions
+Module Module1
+
+    Sub Main()
+#Region "変数宣言"
+        Dim s = ""
+        Dim i = 0
+#End Region
+        Try
+            IntroduceNameOf()
+        Catch ex As ArgumentNullException
+#Disable Warning IDE0002
+            Console.WriteLine(ex.Message)
+#Enable Warning IDE0002
+            Console.WriteLine(ex.Message)
+        End Try
+#Region "これ、どうする？"
+        Dim collectionInitializer = New String({"0"c}) From {"1"c}
+#End Region
+    End Sub
+
+    Private Sub IntroduceNameOf(Optional arg As String = Nothing)
+        If arg Is Nothing Then Throw New ArgumentNullException(NameOf(arg))
+
+    End Sub
+
+End Module
+Namespace Extensions
+    Friend Module SampleExtension
+        <Extension>
+        Friend Sub Add(target As String, param As Char)
+
+        End Sub
+    End Module
+End Namespace
