90e5af10f075c3d83c3e98c3e72573aaba20e2f5
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-07-29 22:22:41 +0900

    Add

--- CenterCLR/Module1.vb
+++ CenterCLR/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Console
+Imports System.Globalization
 Imports System.Runtime.CompilerServices
 Imports CenterCLR.Extensions
 Module Module1
@@ -9,13 +10,15 @@ Module Module1
         Dim i = 0
 #End Region
         Try
+            IntroduceNameOf("Value")
             IntroduceNameOf()
         Catch ex As ArgumentNullException
 #Disable Warning IDE0002
             Console.WriteLine(ex.Message)
 #Enable Warning IDE0002
-            Console.WriteLine(ex.Message)
+            Console.WriteLine("")
         End Try
+        IntroduceStringInterpolation()
 #Region "これ、どうする？"
         Dim collectionInitializer = New String({"0"c}) From {"1"c}
 #End Region
@@ -23,7 +26,19 @@ Module Module1
 
     Private Sub IntroduceNameOf(Optional arg As String = Nothing)
         If arg Is Nothing Then Throw New ArgumentNullException(NameOf(arg))
+        WriteLine($"{NameOf(arg)}:{arg}")
+    End Sub
+    Private Sub IntroduceStringInterpolation(Optional arg As String = Nothing)
+        WriteLine($"{1000:C}")
+        With New Object()
+            Dim format As IFormattable = $"{1000:C}"
+            WriteLine(format.ToString(Nothing, CultureInfo.GetCultureInfo("en-us")))
+        End With
 
+        With New Object()
+            Dim format As IFormattable = FormattableStringFactory.Create("{0:C}", 1000)
+            WriteLine(format.ToString(Nothing, CultureInfo.GetCultureInfo("en-us")))
+        End With
     End Sub
 
 End Module
