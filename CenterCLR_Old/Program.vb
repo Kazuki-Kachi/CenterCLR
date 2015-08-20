7ce696c79d419a6018591602268b8461e2dbf05f
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-08-20 15:25:27 +0900

    Add Old

---
+++ CenterCLR_Old/Program.vb
@@ -0,0 +1,33 @@
+﻿Module Program
+
+    Sub Main()
+
+    End Sub
+
+    <AttributeUsage(AttributeTargets.All, AllowMultiple:=True, Inherited:=False)>
+    Private NotInheritable Class SampleAttribute : Inherits Attribute
+        Public Sub New(data As Object)
+            Me.New(If(data Is Nothing, Nothing, data.ToString()))
+        End Sub
+        Public Sub New(data As String)
+            _data = data
+        End Sub
+
+        Private ReadOnly _data As String
+        Public ReadOnly Property Data As String
+            Get
+                Return _data
+            End Get
+        End Property
+        Public Property DataByInt32 As Integer
+    End Class
+
+    Class Hoge
+        Implements IEquatable(Of Hoge)
+
+        Public Function Equals(other As Hoge) As Boolean Implements IEquatable(Of Hoge).Equals
+
+        End Function
+    End Class
+
+End Module
